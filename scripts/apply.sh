fa62a2d2cd47560b82712a62fbf161ee7db56c0e
Nikola Atanasov <n.atanasov1994@google.com>
2023-06-25 13:41:59 +0200

    Add non working filebeat setup

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 kubectl create namespace scrapewatch
-kubectl apply -f kubernetes --recursive -n scrapewatch
+kubectl apply -f kubernetes/scrapewatch --recursive -n scrapewatch
+kubectl apply -f kubernetes/prometheus --recursive -n scrapewatch
+helm install filebeat kubernetes/filebeat -n scrapewatch

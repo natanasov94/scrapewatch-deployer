0f40006f92f2b2ca3ce225f64c84060b413141e4
Nikola Atanasov <n.atanasov1994@google.com>
2023-07-09 12:49:05 +0200

    Get working local logstash

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -4,3 +4,4 @@ kubectl create namespace scrapewatch
 kubectl apply -f kubernetes/scrapewatch --recursive -n scrapewatch
 kubectl apply -f kubernetes/prometheus --recursive -n scrapewatch
 helm install filebeat kubernetes/filebeat -n scrapewatch
+helm install logstash kubernetes/logstash -n scrapewatch

60d0413dd0332af915853b6c0cbeb9a91d74c814
Nikola Atanasov <n.atanasov1994@google.com>
2023-08-08 20:07:19 +0200

    Fix elasticsearch connection to logstash
    The issue was that logstash needed to connect using the same certificate
    that elasticsearch provided with itself, alongside a user and pass
    setting

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 kubectl create namespace scrapewatch
+
+# Deploy services
 kubectl apply -f kubernetes/scrapewatch --recursive -n scrapewatch
 kubectl apply -f kubernetes/prometheus --recursive -n scrapewatch
 helm install elasticsearch kubernetes/elasticsearch -n scrapewatch
 helm install filebeat kubernetes/filebeat -n scrapewatch
 helm install logstash kubernetes/logstash -n scrapewatch
-# helm install kibana kubernetes/kibana -n scrapewatch
+helm install kibana kubernetes/kibana -n scrapewatch

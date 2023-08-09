73afb5ffa69df70a71f465b16231f05c630788e3
Nikola Atanasov <n.atanasov1994@google.com>
2023-08-09 16:31:24 +0200

    Add metricbeat, along with separately indexed data points in logstash
    Metricbeat is used to transfer over the prometheus data that is
    generated by scrapewatch over to kibana.
    
    Most likely, prometheus will no longer be needed

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -7,5 +7,6 @@ kubectl apply -f kubernetes/scrapewatch --recursive -n scrapewatch
 kubectl apply -f kubernetes/prometheus --recursive -n scrapewatch
 helm install elasticsearch kubernetes/elasticsearch -n scrapewatch
 helm install filebeat kubernetes/filebeat -n scrapewatch
+helm install metricbeat kubernetes/metricbeat -n scrapewatch
 helm install logstash kubernetes/logstash -n scrapewatch
 helm install kibana kubernetes/kibana -n scrapewatch
